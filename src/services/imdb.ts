66dd148a1d7383b7099b8e410140f7e655fc86e8
Zach Kirsch <zachkirsch@gmail.com>
2023-04-13 11:50:50 -0400

    Add fern.config.json comment to FernApi import

--- src/services/imdb.ts
+++ src/services/imdb.ts
@@ -1,23 +1,24 @@
+// this `FernApi` export is generated from your organization name in fern.config.json:
 import { FernApi } from "../api";
 import { ImdbService } from "../api/generated/api/resources/imdb/service/ImdbService";
 
 export default new ImdbService({
-  createMovie: (req, res) => {
-    const id = req.body.title.toLowerCase().replaceAll(" ", "-");
+	createMovie: (req, res) => {
+		const id = req.body.title.toLowerCase().replaceAll(" ", "-");
 
-    // TODO, add movie to database
+		// TODO, add movie to database
 
-    return res.send(id);
-  },
-  getMovie: (req, res) => {
-    if (req.params.movieId === "goodwill-hunting") {
-      res.send({
-        id: req.params.movieId,
-        title: "Goodwill Hunting",
-        rating: 4.9,
-      });
-    } else {
-      throw new FernApi.MovieDoesNotExistError();
-    }
-  },
+		return res.send(id);
+	},
+	getMovie: (req, res) => {
+		if (req.params.movieId === "goodwill-hunting") {
+			res.send({
+				id: req.params.movieId,
+				title: "Goodwill Hunting",
+				rating: 4.9,
+			});
+		} else {
+			throw new FernApi.MovieDoesNotExistError();
+		}
+	},
 });
