f87ebe0e7f7df2fdbfb1ed147bb0287c0caebcdc
Tijani Hussein <60843170+Shield01@users.noreply.github.com>
2023-05-11 06:28:34 +0100

    Enable CORS - Fix to (https://github.com/fern-api/express-starter/issues/11) (#12)
    * Changed the logical OR operator ('||') to a nullish coalescing operator ("??") as it's a safer operator.
    
    Also enabled CORS policy by default.
    
    * Deleted package-lock.json file and ran the "yarn" command.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
+import cors from "cors";
 import express from "express";
 import { register } from "./api/generated";
 import imdb from "./services/imdb";
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT ?? 8080;
 
 const app = express();
+
+//enable cors
+app.use(cors());
 
 register(app, {
   imdb,
