c665c7bf7a116402489f7247b6f495d425dc8ee7
dsinghvi <deep@usebirch.com>
2024-09-04 13:51:45 -0400

    chore(server): upgrade express generator

--- src/api/generated/errors/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FernApiError = void 0;
-var FernApiError_1 = require("./FernApiError");
-Object.defineProperty(exports, "FernApiError", { enumerable: true, get: function () { return FernApiError_1.FernApiError; } });
